393549dd7f18a8c1e265bb9ae587968430341faf
Ken Sodemann <ken@ionic.io>
2020-08-27 11:54:42 -0500

    step(four): access native APIs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,28 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
+import { Plugins } from '@capacitor/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { createPlatformMock } from '../../test/mocks';
 
 describe('AppComponent', () => {
+  let originalSplashScreen: any;
+
   beforeEach(async(() => {
+    originalSplashScreen = Plugins.SplashScreen;
+    Plugins.SplashScreen = jasmine.createSpyObj('SplashScreen', ['hide']);
     TestBed.configureTestingModule({
       declarations: [AppComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      providers: [
-        {
-          provide: StatusBar,
-          useFactory: () =>
-            jasmine.createSpyObj<StatusBar>('StatusBar', ['styleDefault']),
-        },
-        {
-          provide: SplashScreen,
-          useFactory: () =>
-            jasmine.createSpyObj<SplashScreen>('SplashScreen', ['hide']),
-        },
-        { provide: Platform, useFactory: createPlatformMock },
-      ],
+      providers: [{ provide: Platform, useFactory: createPlatformMock }],
     }).compileComponents();
   }));
+
+  afterEach(() => {
+    Plugins.SplashScreen = originalSplashScreen;
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -39,25 +34,28 @@ describe('AppComponent', () => {
     let platform: Platform;
     beforeEach(() => {
       platform = TestBed.inject(Platform);
-      TestBed.createComponent(AppComponent);
     });
 
-    it('waits for the platform to be ready', () => {
-      expect(platform.ready).toHaveBeenCalledTimes(1);
+    describe('in a hybrid mobile context', () => {
+      beforeEach(() => {
+        (platform.is as any).withArgs('hybrid').and.returnValue(true);
+      });
+
+      it('hides the splash screen', () => {
+        TestBed.createComponent(AppComponent);
+        expect(Plugins.SplashScreen.hide).toHaveBeenCalledTimes(1);
+      });
     });
 
-    it('sets the default status bar style when ready', async () => {
-      const statusBar = TestBed.inject(StatusBar);
-      expect(statusBar.styleDefault).not.toHaveBeenCalled();
-      await platform.ready();
-      expect(statusBar.styleDefault).toHaveBeenCalledTimes(1);
-    });
+    describe('in a web context', () => {
+      beforeEach(() => {
+        (platform.is as any).withArgs('hybrid').and.returnValue(false);
+      });
 
-    it('hides the splash screen when ready', async () => {
-      const splashScreen = TestBed.inject(SplashScreen);
-      expect(splashScreen.hide).not.toHaveBeenCalled();
-      await platform.ready();
-      expect(splashScreen.hide).toHaveBeenCalledTimes(1);
+      it('does not hide the splash screen', () => {
+        TestBed.createComponent(AppComponent);
+        expect(Plugins.SplashScreen.hide).not.toHaveBeenCalled();
+      });
     });
   });
 });
